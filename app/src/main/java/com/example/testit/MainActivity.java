5a6da8bfcbd8b799836427c004493d474aad0f67
Sobo_uo43 <Sobo_uo43@student.itstep.org>
2024-10-25 13:56:15 +0300

    Create interface

---
+++ app/src/main/java/com/example/testit/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.testit;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar salarySeekBar;
+    private TextView selectedSalaryText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // находим виджеты
+        salarySeekBar = findViewById(R.id.salary_seekbar);
+        selectedSalaryText = findViewById(R.id.salary_text_view);
+
+        // обработка изменения значения SeekBar для salary
+        SeekBarSalaryListener();
+    }
+
+    private void SeekBarSalaryListener() {
+        salarySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                selectedSalaryText.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+}
